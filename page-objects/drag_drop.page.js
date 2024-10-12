95f72f6a3472797b05fe4e69f69f6fde95575a2d
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-10-12 15:20:11 +0700

    add 2 drag and drop cases

--- page-objects/drag_drop.page.js
+++ page-objects/drag_drop.page.js
@@ -3,8 +3,20 @@ const { browser, expect, $, $$ } = require('@wdio/globals')
 class DragAndDropPage {
   get columnA() { return $('#column-a') }
   get columnB() { return $('#column-b') }
+  get columnAHeader() { return $('#column-a header') }
+  get columnBHeader() { return $('#column-b header') }
 
-  
+  async dragBoxAToColumnB() {
+    await this.columnA.waitForClickable()
+    await this.columnB.waitForDisplayed({timeout:3000})
+    await this.columnA.dragAndDrop(await this.columnB)
+  }
+
+  async dragBoxAToCoordinateColumnB(x, y) {
+    await this.columnA.waitForClickable()
+    await this.columnB.waitForDisplayed({timeout:3000})
+    await this.columnA.dragAndDrop({x:x, y:y})
+  }
 }
 
 module.exports = new DragAndDropPage()
--- test/drag_drop.test.js
+++ test/drag_drop.test.js
@@ -1,4 +1,4 @@
-const { browser, expect } = require('@wdio/globals')
+const { browser, expect, $ } = require('@wdio/globals')
 const mainPage = require('../page-objects/main.page')
 const dragDropPage = require('../page-objects/drag_drop.page')
 
@@ -6,7 +6,24 @@ describe('Drag and Drop', () => {
   it('drag box A to column B', async () => {
     await browser.url('/')
     await mainPage.clickItem(9)
+    await browser.pause(500)
     await expect(browser).toHaveUrl(`${browser.options.baseUrl}/drag_and_drop`)
-    // await browser.pause(3000)
+
+    await dragDropPage.dragBoxAToColumnB()
+    await expect(dragDropPage.columnAHeader).toHaveText('B')
+    await expect(dragDropPage.columnBHeader).toHaveText('A')
+    // await browser.debug()
+    // await expect($('#column-b meta')).toExist()
+  })
+  
+  it('drag box A to column B by using Pixel Coordinate', async () => {
+    await browser.url('/')
+    await browser.maximizeWindow()
+    await mainPage.clickItem(9)
+    await browser.pause(500)
+    await dragDropPage.dragBoxAToCoordinateColumnB(225, 0)    // get pixel coordinate with help of chrome extension "Pixel Measurement"
+    await expect(dragDropPage.columnAHeader).toHaveText('B')
+    await expect(dragDropPage.columnBHeader).toHaveText('A')
+    // await expect($('#column-b meta')).toExist()
   })
 })
