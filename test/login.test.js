0b6e64db20a40cea7569cf61ff4e9ad30530332d
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-10-14 22:28:57 +0700

    add more cases for login page

--- test/login.test.js
+++ test/login.test.js
@@ -3,7 +3,7 @@ const mainPage = require('../page-objects/main.page')
 const { loginPage, securePage} = require('../page-objects/login.page')
 const testData = require('../test-data/user.data')
 
-describe('Login', () => {
+describe('Login Page', () => {
   it('open the login page', async () => {
     await browser.url('/')
     await mainPage.clickItem(20)
@@ -18,6 +18,7 @@ describe('Login', () => {
     await expect(browser).toHaveUrl(`${browser.options.baseUrl}/secure`)
     await expect(securePage.flashMessage$).toHaveText('You logged into a secure area!', { containing:true })
     await expect(securePage.pageHeading$).toHaveText('Secure Area')
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'success'])
   })
 
   // it('close the success flash message', async () => {
@@ -32,5 +33,44 @@ describe('Login', () => {
     await expect(browser).toHaveUrl(`${browser.options.baseUrl}/login`)
     await expect(loginPage.pageHeading$).toHaveText('Login Page')
     await expect(loginPage.flashMessage$).toHaveText('You logged out of the secure area!', { containing:true })
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'success'])
+  })
+
+  it('failed to login due to wrong username', async () => {
+    await browser.url('/login')
+    await loginPage.inputUsername(testData.wrongUsername.USERNAME)
+    await loginPage.inputPassword(testData.wrongUsername.PASSWORD)
+    await loginPage.clickLoginButton()
+    await expect(browser).toHaveUrl(`${browser.options.baseUrl}/login`)
+    await expect(loginPage.flashMessage$).toHaveText('Your username is invalid!', { containing:true })
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'error'])
+  })
+  
+  it('failed to login due to wrong password', async () => {
+    await loginPage.inputUsername(testData.wrongPassword.USERNAME)
+    await loginPage.inputPassword(testData.wrongPassword.PASSWORD)
+    await loginPage.clickLoginButton()
+    await expect(browser).toHaveUrl(`${browser.options.baseUrl}/login`)
+    await expect(loginPage.flashMessage$).toHaveText('Your password is invalid!', { containing:true })
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'error'])  
+  })
+  
+  it('failed to login due to empty username and password', async () => {
+    await loginPage.inputUsername(testData.emptyData.USERNAME)
+    await loginPage.inputPassword(testData.emptyData.PASSWORD)
+    await loginPage.clickLoginButton()
+    await expect(browser).toHaveUrl(`${browser.options.baseUrl}/login`)
+    await expect(loginPage.flashMessage$).toHaveText('Your username is invalid!', { containing:true })
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'error'])
+  })
+
+  it('login successfully after failed login 3x', async () => {
+    await loginPage.inputUsername(testData.validCase.USERNAME)
+    await loginPage.inputPassword(testData.validCase.PASSWORD)
+    await loginPage.clickLoginButton()
+    await expect(browser).toHaveUrl(`${browser.options.baseUrl}/secure`)
+    await expect(securePage.flashMessage$).toHaveText('You logged into a secure area!', { containing:true })
+    await expect(securePage.pageHeading$).toHaveText('Secure Area')
+    await expect(loginPage.flashMessage$).toHaveElementClass(['flash', 'success'])
   })
 })
